13cbe8dfe4b196933a3a9b6e03dc6d1d8327d57d
guilherme <guiduffes@gmail.com>
2023-05-02 23:18:21 -0300

    end commit

---
+++ final.js
@@ -0,0 +1,25 @@
+function salvarImagem() {
+    // seleciono a div que vo salva como png
+    var element = document.getElementById('filhoDown');
+    element.parentNode.style.display = "flex";
+
+
+    html2canvas(element).then(function (canvas) {
+        var link = document.createElement('a');
+        link.download = 'image.png';
+        link.href = canvas.toDataURL();
+        link.target = '_blank';
+        link.click();
+    });
+    
+    element.parentNode.style.display = "none";
+
+};
+
+window.onload = function() {
+    var inputValue = localStorage.getItem('inputValue');
+    document.querySelectorAll('.nome2')[0].innerHTML = inputValue.toUpperCase();
+    document.querySelectorAll('.nome2')[1].innerHTML = inputValue.toUpperCase();
+    var genero = localStorage.getItem('genero');
+    document.querySelector('.genero').textContent = "E VOU SER " + genero
+};
---
+++ form.js
@@ -0,0 +1,41 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var nome = document.querySelector("#nome").value;
+
+    if (nome == "" || nome.length < 3) {
+        alert("O campo 'Nome' é obrigatório e deve conter mais de 2 letras!");
+        return;
+    }
+
+    var checkboxes = document.querySelectorAll('input[type="radio"]');
+    var algumCheckboxSelecionado = false;
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            algumCheckboxSelecionado = true;
+        }
+    });
+    if (!algumCheckboxSelecionado) {
+        alert("Você deve selecionar pelo menos uma opção de 'Sexo'!");
+        return;
+    }
+
+    document.querySelector("form").submit();
+});
+
+var radios = document.querySelectorAll('input[type=radio]');
+
+radios.forEach(function(radio){
+    radio.addEventListener('change', function(){
+        if(this.checked){
+            localStorage.setItem('genero', this.value);
+        }
+    });
+});
+
+document.querySelector('button').addEventListener('click', function(event) {
+    var inputValue = document.querySelector('#nome').value;
+    localStorage.setItem('inputValue', inputValue);
+    var inputSexo = document.querySelector('#SexoM').checked;
+    localStorage.setItem('inputSexo', inputSexo);
+  });
